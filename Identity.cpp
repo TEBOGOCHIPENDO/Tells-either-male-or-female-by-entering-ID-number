35d758c10cfd63f71dd6c7b75c04a7961d9a7c9c
TEBOGOLUCKY <104618629+TEBOGOCHIPENDO@users.noreply.github.com>
2022-05-26 09:43:27 +0200

    Add files via upload

---
+++ Identity.cpp
@@ -0,0 +1,32 @@
+#include  <iostream>
+using namespace  std;
+#include <string>
+#include <cmath>
+
+int main () {
+    string ID;
+
+    cout <<"Enter your ID "<< endl;
+    cin >>  ID;
+    if (ID [6]>4){
+    cout <<"female";
+
+    }else if (ID [6] <4); {
+    cout <<"male";
+
+
+    }
+
+return 0;
+    }
+
+
+
+
+
+
+
+
+
+
+
